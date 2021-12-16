d773e9eb13e683974685e67404f0381915cc5c76
JGaviria <j.gaviria@utp.edu.co>
2021-12-16 16:58:40 -0500

    Check

--- levels.py
+++ levels.py
@@ -74,7 +74,7 @@ class Level_01(Level):
         self.level_limit = -2500
 
         # Array with type of platform, and x, y location of the platform.
-        level = [ [platforms.GRASS_LEFT, 500, 500],
+        level = [  
                   [platforms.GRASS_MIDDLE, 570, 500],
                   [platforms.GRASS_RIGHT, 640, 500],
                   [platforms.GRASS_LEFT, 800, 400],
@@ -85,7 +85,7 @@ class Level_01(Level):
                   [platforms.GRASS_RIGHT, 1140, 500],
                   [platforms.STONE_PLATFORM_LEFT, 1120, 280],
                   [platforms.STONE_PLATFORM_MIDDLE, 1190, 280],
-                  [platforms.STONE_PLATFORM_RIGHT, 1260, 280],
+                  [platforms.STONE_PLATFORM_RIGHT, 14070, 280],
                  
                   ]
 
